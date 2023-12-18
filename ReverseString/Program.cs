705bf61ce201db48e6431fc6ba4a8ea54759fb71
Dean Clarke <dclarke@twilio.com>
2023-12-19 00:25:30 +1030

    Removed methods and implemented a for loop for reversing the string

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace ReverseString
@@ -7,33 +8,26 @@ namespace ReverseString
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("------------------------------------------");
-           Console.Write("Please enter a string to be analyzed: ");
+            string reverseInput = "";
+            
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Please enter a string to be analyzed: ");
+            string userInput = Console.ReadLine().ToLower();
 
-           string userInput = Console.ReadLine().ToLower();
-           string reverseInput = Reverse(userInput).ToLower();
-           bool isInputPalindrome = isPalindrome(userInput, reverseInput);
+            for(int i = userInput.Length -1; i >= 0; i-- ){
+                reverseInput += userInput[i];
+            }
 
-           Console.WriteLine("------------------------------------------");
-           Console.WriteLine($"String entered: {userInput}"); 
-           Console.WriteLine($"String reversed: {reverseInput}");
-           Console.WriteLine($"String is a Palindrome: {isInputPalindrome}");
-           Console.WriteLine("------------------------------------------");
-           Console.WriteLine("Press enter to exit");
-           Console.ReadLine();
-           
-        }
-        public static string Reverse(string word)
-        {
-            char[] strArr = word.ToCharArray();
-            Array.Reverse(strArr);
-            string wordReversed = new String(strArr);
-            return wordReversed;
-        }
+            bool isInputPalindrome = userInput.Equals(reverseInput);
 
-        public static bool isPalindrome(string input, string reverseInput)
-        {
-            return input.Equals(reverseInput);
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"String entered: {userInput}"); 
+            Console.WriteLine($"String reversed: {reverseInput}");
+            Console.WriteLine($"String is a Palindrome: {isInputPalindrome}");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+
         }
     }
 
